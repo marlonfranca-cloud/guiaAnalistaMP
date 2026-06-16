8eadaade925f16c685a084693afb17377212d0fa
Marlon <marlonfranca@multipedidos.com.br>
2026-06-16 14:09:54 -0300

    atualizando api

--- api/logout.js
+++ api/logout.js
@@ -1,7 +1,7 @@
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.setHeader(
     'Set-Cookie',
     'mp_session=; HttpOnly; Secure; SameSite=Strict; Path=/; Max-Age=0'
   );
   res.redirect(302, '/login');
-}
+};
